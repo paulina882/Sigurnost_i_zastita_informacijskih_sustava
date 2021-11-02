f7d61876414882c10285032a4d280d832ae21482
paulina882 <paulina.bakoc@stud.unidu.hr>
2021-11-02 17:14:00 +0100

    promjena u index.py

--- index.py
+++ index.py
@@ -1,1 +1,12 @@
-print('Hello, world!')
+print('Dobrodošli u Unidu sustav')
+print('Za prijavu unesite broj 1, za registraciju broj 2:')
+broj=0
+dopusteni=[1,2]
+while broj not in dopusteni:
+    broj=int(input('Unesite broj:'))
+if broj==1:
+    print("Dobrodošli u Prijavu!")
+if broj==2:
+    print("Dobrodošli u registraciju!")
+
+  
