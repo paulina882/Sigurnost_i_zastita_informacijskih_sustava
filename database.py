139165aa890ce4d5b885e16ae74a2774793d875f
Your Name <you@example.com>
2021-11-23 17:18:35 +0100

    zadnja promjena

--- database.py
+++ database.py
@@ -2,10 +2,13 @@ import sqlite3
 con = sqlite3.connect('baza.db')
 
 cur = con.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS users
+               ( id INTEGER PRIMARY KEY  , name text, email text, password text, kontakt text,  created_at text, br_prijava INTEGER DEFAULT 0)''')
 
-cur.execute('''CREATE TABLE IF NOT EXISTS user
-               (ID INTEGER PRIMARY KEY , name text, email text, password text, kontakt text, created_a text)''')
 
 con.commit()
 
+
 con.close()
+
+
--- index.py
+++ index.py
@@ -4,6 +4,30 @@ print('Dobrodošli u Unidu sustav')
 print('Za prijavu unesite broj 1, za registraciju broj 2:')
 broj=0
 dopusteni=[1,2]
+brojac=0
+
+def login():
+        email = input("Unesi email: ")
+        password = input("Unesi lozinku: ")
+        
+        con=con = sqlite3.connect('baza.db')
+        cur = con.cursor()
+        korisnik=cur.execute("SELECT password,br_prijava,name FROM users WHERE email = ?", (email))
+        korisnik=cur.fetchone();
+        brojac = int(korisnik[1])
+        ime = korisnik[2]
+                        
+        if(korisnik[0]==password):
+                print("Dobrodosao ")
+                print(ime)
+                print("Korisnik postoji i broj prijava je : ")
+                brojac=brojac +1
+                print(brojac)
+                brojac=str(brojac)
+                cur.execute("update users SET br_prijava = ? where email = ?", (brojac ,email))
+                con.commit()
+                con.close()
+        
 def register_user():
         con=con = sqlite3.connect('baza.db')
         cur = con.cursor()
@@ -12,7 +36,7 @@ def register_user():
         password=input('Unesite password:')
         kontakt=input('Unesite kontakt:')
         created_at=date.today()
-        cur.execute("INSERT INTO user(name, email, password, kontakt, created_a) VALUES (?, ?, ?, ?, ?)",(ime, email, password, kontakt, created_at))
+        cur.execute("INSERT INTO users(name, email, password, kontakt, created_at, br_prijava) VALUES (?, ?, ?, ?, ?, ?)",(ime, email, password, kontakt, created_at, 0))
         con.commit()
         con.close()
         
@@ -21,6 +45,7 @@ while broj not in dopusteni:
     broj=int(input('Unesite broj:'))
 if broj==1:
     print("Dobrodošli u Prijavu!")
+    login()
 if broj==2:
     print("Dobrodošli u registraciju!")
     register_user()
